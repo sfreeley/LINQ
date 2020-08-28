d9f9a99f90767ddc9cd170e7e114ac59de37dfa9
Sisi Freeley <liusi33@gmail.com>
2020-08-27 21:52:58 -0500

    groupby

--- Program.cs
+++ Program.cs
@@ -4,6 +4,18 @@ using System.Linq;
 
 namespace linq
 {
+    public class Customer
+    {
+        public string Name { get; set; }
+        public double Balance { get; set; }
+        public string Bank { get; set; }
+    }
+
+    public class MillionairesBankHere
+    {
+        public string BankName { get; set; }
+        public double MillionaireBalance { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
@@ -196,6 +208,39 @@ namespace linq
             {
                 Console.WriteLine(num);
             }
+
+            List<Customer> customers = new List<Customer>()
+            {
+                new Customer() { Name = "Bob Lesman", Balance = 80345.66, Bank = "FTB" },
+                new Customer() { Name = "Joe Landy", Balance = 9284756.21, Bank = "WF" },
+                new Customer() { Name = "Meg Ford", Balance = 487233.01, Bank = "BOA" },
+                new Customer() { Name = "Peg Vale", Balance = 7001449.92, Bank = "BOA" },
+                new Customer() { Name = "Mike Johnson", Balance = 790872.12, Bank = "WF" },
+                new Customer() { Name = "Les Paul", Balance = 8374892.54, Bank = "WF" },
+                new Customer() { Name = "Sid Crosby", Balance = 957436.39, Bank = "FTB" },
+                new Customer() { Name = "Sarah Ng", Balance = 56562389.85, Bank = "FTB" },
+                new Customer() { Name = "Tina Fey", Balance = 1000000.00, Bank = "CITI" },
+                new Customer() { Name = "Sid Brown", Balance = 49582.68, Bank = "CITI" }
+            };
+
+            /*
+    Given the same customer set, display how many millionaires per bank.
+    Ref: https://stackoverflow.com/questions/7325278/group-by-in-linq
+
+    Example Output:
+    WF 2
+    BOA 1
+    FTB 1
+    CITI 1
+*/
+            List<MillionairesBankHere> millionairesPerBank = (from customer in customers group customer by customer.Bank into millionairesBank select new MillionairesBankHere
+            {
+                BankName = millionairesBank.Key,
+                    MillionaireBalance = millionairesBank.Count()
+            }).ToList();
+
+            Console.WriteLine("Bank Name and how many millionaires bank there:");
+            millionairesPerBank.ForEach(millionaire => Console.WriteLine($"{millionaire.BankName} {millionaire.MillionaireBalance}"));
 
         }
 
